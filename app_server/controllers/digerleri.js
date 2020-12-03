2ae62a636eae543cd3dac271ab5260049f1a711a
Tarik Kircali <tarikkircali5@gmail.com>
2020-12-03 20:26:03 +0300

    versiyon 2

--- app_server/controllers/digerleri.js
+++ app_server/controllers/digerleri.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 //anasayfayı yöneten metot
  const hakkinda = function(req, res, next) {
-  res.render('hakkinda', { title: 'Hakkında' });
+  res.render('hakkinda', { 
+    title: 'Hakkında',
+    'footer': 'Tarık Kırcalı'
+   });
 }
 
 module.exports={
